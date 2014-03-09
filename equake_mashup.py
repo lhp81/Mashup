dd09256332b07754521c3b13e3379df2b8f65aef
James White <jwhite007@comcast.net>
2014-03-09 14:49:28 -0700

    Added variability to get_city_loc function and added iteration of a list of cities to earthquake function.

--- equake_mashup.py
+++ equake_mashup.py
@@ -14,50 +14,37 @@ def earthquake():
     url = 'http://www.seismi.org/api/eqs/2013?min_magnitude=7'
     resp = requests.get(url)
     data = json.loads(resp.text)
-    lat_c = get_city_loc()[0]
-    lng_c = get_city_loc()[1]
-    # if data['status'] == 'OK':
-    # for i in range(len(data['earthquakes'])):
-    for i in range(2):
-        lat_e = float(data['earthquakes'][i]['lat'])
-        lng_e = float(data['earthquakes'][i]['lon'])
-        mag = float(data['earthquakes'][i]['magnitude'])
-        depth = float(data['earthquakes'][i]['depth'])
-        #  Haversine calculation for distance using coordinates.
-        R = 6371
-        dLat = rad(lat_c - lat_e)
-        dLon = rad(lng_c - lng_e)
-        lat_er = rad(lat_e)
-        lat_cr = rad(lat_c)
-        a = sin(dLat/2) * sin(dLat/2) + sin(dLon/2) * sin(dLon/2) * cos(lat_er) * cos(lat_cr)
-        c = 2 * atan2(sqrt(a), sqrt(1-a))
-        epi_d = R * c
-        depth_d = sqrt(depth**2 + epi_d**2)
-        intensity = 10000 * mag / depth_d
-        # print("Distance from Seattle: " + str(d) + "\n" + "Magnitude: " + str(mag) + "\n")
-        print("Distance from Los Angeles: " + str(epi_d) + "\n" +
-              "Depth distance from Los Angeles: " + str(depth_d) + "\n" +
-              "Magnitude: " + str(mag) + "\n" +
-              "Intensity: " + str(intensity) + "\n")
-        # print(lat_c, lng_c, lat_e, lng_e, mag, depth)
+    city_list = ['Vancouver, BC', 'Seattle', 'Portland, OR', 'San Francisco, CA', 'Los Angeles, CA']
+    for city in city_list:
+        lat_c = get_city_loc(city)[0]
+        lng_c = get_city_loc(city)[1]
+        # for i in range(len(data['earthquakes'])):
+        for i in range(2):
+            lat_e = float(data['earthquakes'][i]['lat'])
+            lng_e = float(data['earthquakes'][i]['lon'])
+            mag = float(data['earthquakes'][i]['magnitude'])
+            depth = float(data['earthquakes'][i]['depth'])
+            #  Haversine calculation for distance using coordinates.
+            R = 6371
+            dLat = rad(lat_c - lat_e)
+            dLon = rad(lng_c - lng_e)
+            lat_er = rad(lat_e)
+            lat_cr = rad(lat_c)
+            a = sin(dLat/2) * sin(dLat/2) + sin(dLon/2) * sin(dLon/2) * cos(lat_er) * cos(lat_cr)
+            c = 2 * atan2(sqrt(a), sqrt(1-a))
+            epi_d = R * c
+            depth_d = sqrt(depth**2 + epi_d**2)
+            intensity = 10000 * mag / depth_d
+            print("Distance from " + city + ": " + str(epi_d) + "\n" +
+                  "Depth distance from " + city + ": " + str(depth_d) + "\n" +
+                  "Magnitude: " + str(mag) + "\n" +
+                  "Intensity: " + str(intensity) + "\n")
 
-#         var R = 6371; // km
-# var dLat = (lat2-lat1).toRad();
-# var dLon = (lon2-lon1).toRad();
-# var lat1 = lat1.toRad();
-# var lat2 = lat2.toRad();
 
-# var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-#         Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-# var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-# var d = R * c;
-    # print(data['count'])
-    # print(len(data['earthquakes']))
-    # print(len(data))
-
-def get_city_loc():
-    # url = 'http://maps.google.com/maps/api/geocode/json?address=seattle&sensor=false'
-    url = 'http://maps.google.com/maps/api/geocode/json?address=los+angeles&sensor=false'
+def get_city_loc(city):
+    city_list = city.split()
+    city_entry = "+".join(city_list)
+    url = 'http://maps.google.com/maps/api/geocode/json?address=' + city_entry + '&sensor=false'
     resp = requests.get(url)
     data = json.loads(resp.text)
     lat = data['results'][0]['geometry']['location']['lat']
