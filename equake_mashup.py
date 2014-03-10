3fac81a600543e840dec3e75ccbb6f7e265c40f8
James White <jwhite007@comcast.net>
2014-03-09 17:47:28 -0700

    Everything all together now.... my work, Luke's work, and the main wsgi handler.

--- equake_mashup.py
+++ equake_mashup.py
@@ -10,6 +10,19 @@ from math import radians as rad
 from math import sqrt as sqrt
 
 
+def resolve_path(path):
+    urls = [(r'^$', cities),
+            (r'^^/cities/(id[\d]+)$', city)]
+    matchpath = path.lstrip('/')
+    for regexp, func in urls:
+        match = re.match(regexp, matchpath)
+        if match is None:
+            continue
+        args = match.groups([])
+        return func, args
+    raise NameError
+
+
 def earthquake():
     url = 'http://www.seismi.org/api/eqs/2013?min_magnitude=7'
     resp = requests.get(url)
@@ -22,8 +35,6 @@ def earthquake():
         intensity_list = []
         eq_dict = {}
         for i in range(len(data['earthquakes'])):
-        # print(city + ":")
-        # for i in range(2):
             stri = str(i + 1)
             timedate = data['earthquakes'][i]['timedate']
             lat_e = float(data['earthquakes'][i]['lat'])
@@ -67,6 +78,56 @@ def get_city_loc(city):
     lat = data['results'][0]['geometry']['location']['lat']
     lng = data['results'][0]['geometry']['location']['lng']
     return (lat, lng)
+
+
+def cities():
+    all_cities = [key for key in earthquake()]
+    all_means = earthquake()['city'][0] for city in all_cities
+    body = ['<h1>West Coast City Earthquake Data</h1>', '<ul>']
+    item_template = ('<li><strong><a href="/cities/{city}">{city}</a></strong>'
+                     '(Mean Intensity: {mean_intensity})</li>')
+    for city in all_cities:
+        body.append(item_template.format(**city,**mean_intensity)))
+    body.append('</ul>')
+    return '\n'.join(body)
+
+
+def city(city):
+    page = """
+<h1>{city}</h1>
+<table>
+    <tr><th>Distance from {city}:</th><td>{city_dict[{city}][1]['1'][1]}</td></tr>
+    <tr><th>Depth Distance from {city}:</th><td>{city_dict[{city}][1]['1'][2]}</td></tr>
+    <tr><th>Magnitude:</th><td>{city_dict[{city}][1]['1'][3]}</td></tr>
+    <tr><th>Intensity:</th><td>{city_dict[{city}][1]['1'][4]}</td></tr>
+</table>
+<a href="/">Back to the list</a>
+"""
+    city = DB.title_info(city)
+    if city is None:
+        raise NameError
+    return page.format(**city)
+
+
+def application(environ, start_response):
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body]
 
 if __name__ == '__main__':
     earthquake()
